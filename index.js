adfc478fba7ca3a98c3afd65071870db41d3dccc
Anand Chauhan <79563144+acmax406@users.noreply.github.com>
2022-11-04 15:19:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+document.write('<h3> welcome to my firstpage </h3>');
+document.write("<br/>");
+document.write("Tank you ...!");
+
+var x = 24;
+
+var y = 22.5;
+
+var str = 'anand chauhan'
+
+document.write("<br/>");
+document.write("value is:",+x+'<br/>');
+document.write("value is:"+ y+ "<br/>");
+document.write("sum is:",+x+y+'<br/>');
+
+document.write("boolean values is:"+ (100>1003)+"<br/>");
